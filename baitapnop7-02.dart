6adf4f73d3f911eebde82632fa2bf67d80d71b79
BiiPhone <biiphoneprinter@gmail.com>
2025-08-17 11:33:04 +0700

    update

--- baitapnop7-02.dart
+++ baitapnop7-02.dart
@@ -8,6 +8,7 @@ void main() {
   print("Vui Lòng Nhập Giá Tiền Sản Phẩm");
   double price = double.tryParse(stdin.readLineSync()!)!;
   var totalprice = slsp * price;
+
   double giamgia = 0;
   if (totalprice >= 1_000_000) {
     giamgia = totalprice * 10 / 100;
@@ -18,8 +19,14 @@ void main() {
   if (totalprice < 500_000) {
     giamgia = 0;
   }
-  totalprice = totalprice - giamgia;
+  double totalpricegiamgia = totalprice - giamgia;
+  double VAT = totalpricegiamgia * 8 / 100;
+  double bill = totalpricegiamgia+VAT;
   print('Tên Sản Phẩm : $namesp');
   print('Tổng Số Lượng : $slsp');
-  print('Tổng Bill : $totalprice');
+  print('Giá Sản Phẩm : $price');
+  print('Tổng Tiền : $totalprice');
+  print('Tổng Tiền Được Giảm : $totalpricegiamgia');
+  print('VAT : $VAT');
+  print('BILL : $bill');
 }
